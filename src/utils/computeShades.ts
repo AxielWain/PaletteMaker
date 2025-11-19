47837479aaed7d9584092381a1964f977402e997
Axiel Wain <243482860+AxielWain@users.noreply.github.com>
2025-11-19 07:15:30 -0500

    Revamp palette computation
    - Interpolate light and dark shades using current lightness as source
    - Implement hue shifting by interpolating using a max, min and current
      lightness

--- src/utils/computeShades.ts
+++ src/utils/computeShades.ts
@@ -1,42 +1,50 @@
-import { darken, lighten, parseToHsl } from 'polished'
+import { darken, lighten, parseToHsl, toColorString } from 'polished'
 import { TRANSPARENT_COLOR } from './constants'
 
-const higherLightnessThreshold = 0.9
-const lowerLightnessThreshold = 0.1
+const linearInterpolation = (a: number, b: number, t: number) => {
+  return (b - a) * t + a
+}
 
-export function computeShades(color: string, maxShades = 6) {
-  const { lightness } = parseToHsl(color)
-  // max lighter shades plus max darker shades plus the base color row
-  const totalVerticalColors = maxShades * 2 + 1
-  const deltaChange = 1 / totalVerticalColors
-  const darkerShades: string[] = []
-  const lighterShades: string[] = []
-  let darkerLuminance = lightness
-  let lighterLuminance = lightness
+const normalize = (value: number, min: number, max: number) => {
+  return (value - min) / (max - min)
+}
 
-  for (let i = 0; i < maxShades; i++) {
-    if (darkerLuminance >= lowerLightnessThreshold) {
-      darkerLuminance -= deltaChange
-      darkerShades.push(darken(deltaChange * (darkerShades.length + 1), color))
-    }
+export function computeShades(
+  color: string,
+  maxShades = 6,
+  maxLightness = 0.9,
+  minLightness = 0.1
+) {
+  const { hue, saturation, lightness } = parseToHsl(color)
+  const shades: string[] = []
+  const deltaLightness = (maxLightness - minLightness) / 12
+  let accuLightness = maxLightness
 
-    if (lighterLuminance <= higherLightnessThreshold) {
-      lighterLuminance += deltaChange
-      lighterShades.unshift(
-        lighten(deltaChange * (lighterShades.length + 1), color)
+  for (let i = 0; i < 12; i++) {
+    const t = normalize(accuLightness, minLightness, maxLightness)
+    const newLightness =
+      accuLightness > lightness
+        ? linearInterpolation(lightness, maxLightness, t)
+        : linearInterpolation(minLightness, lightness, t)
+    const newHue =
+      hue +
+      linearInterpolation(
+        -30,
+        30,
+        normalize(newLightness, minLightness, maxLightness)
       )
-    }
+    shades.push(
+      toColorString({
+        hue: newHue,
+        saturation,
+        lightness: newLightness,
+      })
+    )
+    accuLightness -= deltaLightness
   }
 
-  const fillerDarkerShades = maxShades - darkerShades.length
-  for (let i = 0; i < fillerDarkerShades; i++) {
-    darkerShades.push(TRANSPARENT_COLOR)
-  }
-
-  const fillerLighterShades = maxShades - lighterShades.length
-  for (let i = 0; i < fillerLighterShades; i++) {
-    lighterShades.unshift(TRANSPARENT_COLOR)
-  }
+  const lighterShades = shades.slice(0, maxShades)
+  const darkerShades = shades.slice(maxShades, shades.length)
 
   return { darkerShades, lighterShades }
 }
